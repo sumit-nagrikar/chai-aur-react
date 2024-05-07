bca1c881b91604bba2101b997022e5b56b60daf2
sumit-nagrikar <148752727+sumit-nagrikar@users.noreply.github.com>
2024-05-07 16:58:59 +0530

    Update main.jsx

--- 02counter/src/main.jsx
+++ 02counter/src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+//This  is createRoot method inside ReactDOM and we are searching root elemement and rendering function
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
