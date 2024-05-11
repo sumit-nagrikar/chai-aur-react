fe6b87f8c83e14557760cca9e7c8c3fa1ef09635
sumit-nagrikar <148752727+sumit-nagrikar@users.noreply.github.com>
2024-05-11 14:13:54 +0530

    Update App.jsx added new comments for better readibility

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -4,13 +4,12 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-
-  const [counter, setCounter]  = useState(15)
-
-  //let counter = 15
+//created state variable and set method/update method
+  const [counter, setCounter]  = useState(15)// 15 is a initial state value
 
   const addValue = () => {
     //counter = counter + 1
+    //set method takes a callback function in usestate, like this callback give use previously updated value and we can work with that in next step
     setCounter(prevCounter => prevCounter + 1)
     setCounter(prevCounter => prevCounter + 1 )
     setCounter(prevCounter => prevCounter + 1)
@@ -19,14 +18,19 @@ function App() {
   }
 
   const removeValue = () => {
+    //if we directly wrote like this always work with the initial state value
     setCounter(counter - 1)
   }
   
-  return (
-    <>
+  return (//here we are returnig a html which will renderd on the webpage in div root
+    <>       //fragments
+      //heading
       <h1>Chai aur react</h1>
+
+      //cuurrent counter value
       <h2>Counter value: {counter}</h2>
 
+      //addValue button which will increase the counter value 
       <button
       onClick={addValue}
       >Add value {counter}</button> 
