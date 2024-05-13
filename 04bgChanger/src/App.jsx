e72c5149a3b2b4886d9af12963a1ff1563601373
sumit-nagrikar <148752727+sumit-nagrikar@users.noreply.github.com>
2024-05-13 10:47:05 +0530

    Update App.jsx

--- 04bgChanger/src/App.jsx
+++ 04bgChanger/src/App.jsx
@@ -2,15 +2,20 @@ import { useState } from "react"
 
 
 function App() {
-  const [color, setColor] = useState("olive")
+  //state variable to save initial bg-color and set method to update color
+  const [color, setColor] = useState("olive")  
 
   return (
+    //main screen size div
     <div className="w-full h-screen duration-200"
     style={{backgroundColor: color}}
     >
+      //div for creating a block there we are adding button elements
       <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
+
+        //button elements
         <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl">
-          <button
+          <button//here we changed / updated the initial color by using setmethod
           onClick={() => setColor("red")}
           className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
           style={{backgroundColor: "red"}}
