70e7cbe74171e4aa8e3cbbb083db1c5c8b531877
sumit-nagrikar <148752727+sumit-nagrikar@users.noreply.github.com>
2024-05-15 16:54:34 +0530

    Update useCurrencyInfo.js
    added comments to understand custom hook

--- 06currencyConvertor/src/hooks/useCurrencyInfo.js
+++ 06currencyConvertor/src/hooks/useCurrencyInfo.js
@@ -1,8 +1,12 @@
 import {useEffect, useState} from "react"
 
-
+//custome hook
 function useCurrencyInfo(currency){
+    //usestate variable
     const [data, setData] = useState({})
+
+    //useeffect take one callback and a dependency array
+    //we are doing this for if by any chance the currency change code inside useeffect will get executed and recreated again
     useEffect(() => {
         fetch(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`)
         .then((res) => res.json())
@@ -10,7 +14,9 @@ function useCurrencyInfo(currency){
         console.log(data);
     }, [currency])
     console.log(data);
+    //it is returning data
     return data
 }
-
+//now we are exoporting this custom hook so that we could use it wherever we want get the data
+//we just need to pass particualr currency and we got the data we want
 export default useCurrencyInfo;
