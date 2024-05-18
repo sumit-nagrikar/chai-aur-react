5cedf92314877a6df1c1312fd49029ffdaf8b266
sumit-nagrikar <148752727+sumit-nagrikar@users.noreply.github.com>
2024-05-18 14:19:37 +0530

    Update Github.jsx

--- 07reactRouter/src/components/Github/Github.jsx
+++ 07reactRouter/src/components/Github/Github.jsx
@@ -1,9 +1,9 @@
 import React, { useEffect, useState } from 'react'
-import { useLoaderData } from 'react-router-dom'
+import { useLoaderData } from 'react-router-dom'//used to load data faster with APIs
 
 function Github() {
-    const data = useLoaderData()
-    // const [data, setData] = useState([])
+    const data = useLoaderData()//this is a state
+    // const [data, setData] = useState([])              //This will load the data after clicking of navlink like home about
     // useEffect(() => {
     //  fetch('https://api.github.com/users/hiteshchoudhary')
     //  .then(response => response.json())
@@ -22,6 +22,7 @@ function Github() {
 
 export default Github
 
+//To load data in more optimizes way
 export const githubInfoLoader = async () => {
     const response = await fetch('https://api.github.com/users/hiteshchoudhary')
     return response.json()
