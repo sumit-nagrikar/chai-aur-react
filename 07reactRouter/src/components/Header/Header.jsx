167c32155ccd6172a13db0ef51f23ce3a74724ff
sumit-nagrikar <148752727+sumit-nagrikar@users.noreply.github.com>
2024-05-18 11:59:51 +0530

    Update Header.jsx

--- 07reactRouter/src/components/Header/Header.jsx
+++ 07reactRouter/src/components/Header/Header.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
-import {Link, NavLink} from 'react-router-dom'
+import {Link, NavLink} from 'react-router-dom'//In react we use Link instead of anchor tag cause anchor tag repaint the whole webpage
 
+//Header component here we create the navigation bar and render inside Layout.jsx
 export default function Header() {
     return (
         <header className="shadow sticky z-50 top-0">
             <nav className="bg-white border-gray-200 px-4 lg:px-6 py-2.5">
+
+                //logo
                 <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
                     <Link to="/" className="flex items-center">
                         <img
@@ -13,9 +16,12 @@ export default function Header() {
                             alt="Logo"
                         />
                     </Link>
+
+                    //rightmost part of navbar 
                     <div className="flex items-center lg:order-2">
                         <Link
                             to="#"
+                            //# means link will not lead to another page
                             className="text-gray-800 hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none"
                         >
                             Log in
@@ -27,6 +33,8 @@ export default function Header() {
                             Get started
                         </Link>
                     </div>
+
+                    // Middle part contains HOME ABOUT CONTACT GITHUB links
                     <div
                         className="hidden justify-between items-center w-full lg:flex lg:w-auto lg:order-1"
                         id="mobile-menu-2"
@@ -34,6 +42,7 @@ export default function Header() {
                         <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
                             <li>
                                 <NavLink
+                                    // This "/" means home page
                                 to="/"
                                     className={({isActive}) =>
                                         `block py-2 pr-4 pl-3 duration-200 ${isActive ? "text-orange-700" : "text-gray-700"} border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
@@ -44,7 +53,10 @@ export default function Header() {
                             </li>
                             <li>
                                 <NavLink
-                                to="/about"
+                                to="/about" //this will lead to About page
+                                    /*isActive ? "text-orange-700" : "text-gray-700":
+If isActive is true, the text color is set to orange (text-orange-700).
+If isActive is false, the text color is set to gray (text-gray-700).*/
                                     className={({isActive}) =>
                                         `block py-2 pr-4 pl-3 duration-200 ${isActive ? "text-orange-700" : "text-gray-700"} border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
                                     }
