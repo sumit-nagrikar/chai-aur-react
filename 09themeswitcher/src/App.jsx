d1fe9c0a8af81364790ca4d927282077b766b272
sumit-nagrikar <148752727+sumit-nagrikar@users.noreply.github.com>
2024-05-19 23:17:54 +0530

    Update App.jsx

--- 09themeswitcher/src/App.jsx
+++ 09themeswitcher/src/App.jsx
@@ -6,6 +6,7 @@ import ThemeBtn from './components/ThemeBtn'
 import Card from './components/Card'
 
 function App() {
+  //actual varible and method to show changes in state
   const [themeMode, setThemeMode] = useState("light")
 
   const lightTheme = () => {
@@ -19,12 +20,15 @@ function App() {
   // actual change in theme
 
   useEffect(() => {
+    //find the element and remove the classmname light and dark cause we need to make it empty so that we can apply the mode we want that mensa the class we want
     document.querySelector('html').classList.remove("light", "dark")
     document.querySelector('html').classList.add(themeMode)
   }, [themeMode])
-  
+  //dependency array 
 
   return (
+    //wrapped the jsx with ThemeProvider so now the access is taken
+    //But these are empty methods we declared inside components and we can define them here and definition will directly go to the actual method
     <ThemeProvider value={{themeMode, lightTheme, darkTheme}}>
       <div className="flex flex-wrap min-h-screen items-center">
           <div className="w-full">
