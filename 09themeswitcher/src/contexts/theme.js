8c6e8bc9ffa31e6f79526674038f6abee061383c
sumit-nagrikar <148752727+sumit-nagrikar@users.noreply.github.com>
2024-05-19 23:09:47 +0530

    Update theme.js

--- 09themeswitcher/src/contexts/theme.js
+++ 09themeswitcher/src/contexts/theme.js
@@ -1,13 +1,17 @@
 import { createContext, useContext } from "react";
 
+created context and provided it with a object
 export const ThemeContext = createContext({
     themeMode: "light",
     darkTheme: () => {},
     lightTheme: () => {},
 })
 
+// created provider and exported it with a variable
 export const ThemeProvider = ThemeContext.Provider
 
+//exported a function by using this fn we use the context everywhere we want
+//just need to import this useTheme and we can use the insider variables and methods
 export default function useTheme(){
     return useContext(ThemeContext)
 }
