235050bf6e690be31e3b6e1049e10570151d1e41
sumit-nagrikar <148752727+sumit-nagrikar@users.noreply.github.com>
2024-05-19 15:06:21 +0530

    Context summary

--- 08miniContext/src/context/UserContext.js
+++ 08miniContext/src/context/UserContext.js
@@ -1,6 +1,12 @@
 import React from 'react'
 
+//context created from react and exported
 const UserContext = React.createContext()
 
 export default UserContext;
 
+/*Summary
+Create Context: const UserContext = React.createContext();
+Provide Context: Use UserContext.Provider to make the context value available to the component tree.
+Consume Context: Use useContext(UserContext) in any component that needs access to the context.
+By following these steps, you can effectively manage and share state across your React application without prop drilling.*/
