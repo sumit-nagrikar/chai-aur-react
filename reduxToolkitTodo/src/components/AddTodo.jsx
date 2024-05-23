875fa5401c131dfdbb2f759f59a6e64dfc419746
sumit-nagrikar <148752727+sumit-nagrikar@users.noreply.github.com>
2024-05-23 15:24:15 +0530

    Update AddTodo.jsx

--- reduxToolkitTodo/src/components/AddTodo.jsx
+++ reduxToolkitTodo/src/components/AddTodo.jsx
@@ -1,16 +1,18 @@
 import React, {useState} from 'react'
 import {useDispatch} from 'react-redux'
-import {addTodo} from '../features/todo/todoSlice' 
+import {addTodo} from '../features/todo/todoSlice' //retrive the method from slice
 
 function AddTodo() {
 
+    //to show current state of addTodo
     const [input, setInput] = useState('')
+    //imported dispatch to dispatch action methods from reducers from slice
     const dispatch = useDispatch()
 
     const addTodoHandler = (e) => {
         e.preventDefault()
-        dispatch(addTodo(input))
-        setInput('')
+        dispatch(addTodo(input))//go to the action .payload and passed to todo text
+        setInput('')//clear the input todo box
     }
 
   return (
@@ -20,7 +22,7 @@ function AddTodo() {
         className="bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
         placeholder="Enter a Todo..."
         value={input}
-        onChange={(e) => setInput(e.target.value)}
+        onChange={(e) => setInput(e.target.value)}//set inputed text in input state
       />
       <button
         type="submit"
