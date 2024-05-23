2bcaa05deb12e87f2afb86dfef0f2d99a151f1a2
sumit-nagrikar <148752727+sumit-nagrikar@users.noreply.github.com>
2024-05-23 15:16:34 +0530

    Update todoSlice.js

--- reduxToolkitTodo/src/features/todo/todoSlice.js
+++ reduxToolkitTodo/src/features/todo/todoSlice.js
@@ -1,15 +1,18 @@
+
 import {createSlice, nanoid } from '@reduxjs/toolkit';
 
+//initial state of todos
 const initialState = {
     todos: [{id: 1, text: "Hello world"}]
 }
 
 
-
+//slice takes name, initialState and reducers
 export const todoSlice = createSlice({
     name: 'todo',
     initialState,
     reducers: {
+        //here we define action methods
         addTodo: (state, action) => {
             const todo = {
                 id: nanoid(), 
@@ -22,7 +25,8 @@ export const todoSlice = createSlice({
         },
     }
 })
-
+//here we are exporting action methods
 export const {addTodo, removeTodo} = todoSlice.actions
 
+//here we are exporting reducer
 export default todoSlice.reducer
